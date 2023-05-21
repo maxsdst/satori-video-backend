69a1ae360e34c1bd1f8c7a3f505d4808cbbde305
Unnamed <unnamed@undefined.com>
2023-05-21 16:47:42 +0200

    Create video model

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="videos"
+    )
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=2000)
+    source = models.FileField(upload_to="videos")
+    thumbnail = models.ImageField(upload_to="thumbnails")
