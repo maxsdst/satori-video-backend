f679c95c897b316989644ebf959a0194cf8365e3
Unnamed <unnamed@undefined.com>
2023-05-26 15:44:30 +0200

    Add thumbnails to videos

--- videos/utils.py
+++ videos/utils.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import ffmpeg
 import ffmpeg_streaming
 from django.conf import settings
 from django.core.files.storage import default_storage
@@ -22,6 +23,32 @@ def make_hls(input: Path, output_folder: Path) -> Path:
     hls = video.hls(ffmpeg_streaming.Formats.h264())
     hls.auto_generate_representations()
     hls.output(str(output))
+
+    return output
+
+
+def create_thumbnail(input: Path, output_folder: Path) -> Path:
+    """
+    Creates thumbnail for video and writes it to output_folder.
+    Returns path to thumbnail.
+
+    Parameters:
+        input (Path): Path to video
+        output_folder (Path): Path to output folder
+    """
+
+    time = "00:00:00"  # first frame
+    width = "405"  # 405px x 720px (9:16 ratio)
+    file_name = "thumbnail.jpg"
+
+    output = output_folder / file_name
+
+    (
+        ffmpeg.input(str(input), ss=time)
+        .filter("scale", width, -1)
+        .output(str(output), vframes=1)
+        .run()
+    )
 
     return output
 
--- videos/views.py
+++ videos/views.py
@@ -8,7 +8,7 @@ from rest_framework.viewsets import ModelViewSet
 
 from .models import Upload, Video
 from .serializers import CreateUploadSerializer, UploadSerializer, VideoSerializer
-from .utils import get_media_url, make_hls
+from .utils import create_thumbnail, get_media_url, make_hls
 
 
 class VideoViewSet(ModelViewSet):
@@ -45,11 +45,15 @@ class UploadViewSet(ModelViewSet):
             thumbnail="",
         )
 
-        hls_playlist_path = make_hls(
-            Path(upload.file.path), settings.MEDIA_ROOT / "videos" / str(video.id)
-        )
+        video_path = Path(upload.file.path)
+        output_folder = settings.MEDIA_ROOT / "videos" / str(video.id)
 
+        hls_playlist_path = make_hls(video_path, output_folder)
         video.source = get_media_url(hls_playlist_path)
+
+        thumbnail_path = create_thumbnail(video_path, output_folder)
+        video.thumbnail = get_media_url(thumbnail_path)
+
         video.save()
 
         upload.video = video
