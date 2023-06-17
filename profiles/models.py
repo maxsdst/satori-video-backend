30b4963466086d1b13034847e484f363c03dc3de
Unnamed <unnamed@undefined.com>
2023-06-17 04:01:43 +0200

    Allow profile description to be blank

--- profiles/models.py
+++ profiles/models.py
@@ -5,5 +5,5 @@ from django.conf import settings
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=50)
-    description = models.CharField(max_length=250)
+    description = models.CharField(max_length=250, blank=True)
     avatar = models.ImageField(upload_to="avatars", null=True, blank=True)
