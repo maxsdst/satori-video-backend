8e367ceb7c82a645ef699d5008c04d54e41c027c
Unnamed <unnamed@undefined.com>
2023-06-18 03:10:40 +0200

    Optimize database queries in profile viewset

--- profiles/views.py
+++ profiles/views.py
@@ -17,7 +17,7 @@ USER_MODEL = get_user_model()
 
 class ProfileViewSet(RetrieveModelMixin, GenericViewSet):
     http_method_names = ["get", "patch", "head", "options"]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.select_related("user").all()
     serializer_class = ProfileSerializer
 
     @action(
@@ -48,6 +48,8 @@ class ProfileViewSet(RetrieveModelMixin, GenericViewSet):
         url_path="retrieve_by_username/(?P<username>.+)",
     )
     def retrieve_by_username(self, request: Request, username: str):
-        profile = get_object_or_404(Profile, user__username=username)
+        profile = get_object_or_404(
+            Profile.objects.select_related("user"), user__username=username
+        )
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
