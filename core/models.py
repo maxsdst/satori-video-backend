06be918866bd15c828e6f31cc9807fc7ca9f3d14
Unnamed <unnamed@undefined.com>
2023-11-21 15:59:19 +0100

    Select user's related profile in each request

--- core/models.py
+++ core/models.py
@@ -1,6 +1,13 @@
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+
+
+class CustomUserManager(UserManager):
+    def get(self, *args, **kwargs):
+        return super().select_related("profile").get(*args, **kwargs)
 
 
 class User(AbstractUser):
+    objects = CustomUserManager()
+
     email = models.EmailField(unique=True)
