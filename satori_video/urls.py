9ec403ef93cb466e48a6c8dd438f4630959ea9b8
Unnamed <unnamed@undefined.com>
2024-10-09 07:59:56 +0200

    Add health check endpoint

--- satori_video/urls.py
+++ satori_video/urls.py
@@ -22,6 +22,7 @@ from django.urls import include, path
 
 
 urlpatterns = [
+    path("", include("core.urls")),
     path("admin/", admin.site.urls),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
